8773c3903ba9ef5cbbfe9d1ba733b1c5023793e3
Angel-Mathew007 <140804005+Angel-Mathew007@users.noreply.github.com>
2024-06-08 09:06:22 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -27,32 +27,50 @@ class Graph extends Component<IProps, {}> {
       top_ask_price: 'float',
       top_bid_price: 'float',
       timestamp: 'date',
+      ratio: 'float',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
+    
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
+    
+    if (window.perspective && window.perspective.worker) {
+      const worker = window.perspective.worker();
+      this.table = worker.table(schema);
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "upper_bound", "lower_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
+        stock: 'distinct count',
         top_ask_price: 'avg',
-        top_bid_price: 'avg',
         timestamp: 'distinct count',
+        ratio: 'avg',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
+  }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update(this.props.data.map((el: any) => {
+        return {
+          stock: el.stock,
+          top_ask_price: el.top_ask && el.top_ask.price || 0,
+          timestamp: el.timestamp,
+          ratio: el.ratio,
+          upper_bound: el.upper_bound,
+          lower_bound: el.lower_bound,
+          trigger_alert: el.trigger_alert,
+        };
+      }));
     }
   }
 }
